30ee64c26b841b343db1b7d958ace806bf0ac74f
Yandong Mao <ydmao@mit.edu>
2012-10-27 18:12:34 -0400

    add little license header

--- lib/ibs.cc
+++ lib/ibs.cc
@@ -1,3 +1,17 @@
+/* Metis
+ * Yandong Mao, Robert Morris, Frans Kaashoek
+ * Copyright (c) 2012 Massachusetts Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, subject to the conditions listed
+ * in the Metis LICENSE file. These conditions include: you must preserve this
+ * copyright notice, and you cannot mention the copyright holders in
+ * advertising related to the Software without their permission.  The Software
+ * is provided WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED. This notice is a
+ * summary of the Metis LICENSE file; the license in that file is legally
+ * binding.
+ */
 #include <stdio.h>
 #include <fcntl.h>
 #include <assert.h>
